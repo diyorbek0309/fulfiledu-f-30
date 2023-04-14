5f102c53f0564475e41b7a6b7c93dda906c6349d
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2023-04-14 22:37:09 +0500

    lesson 21

---
+++ 21-dars/main.js
@@ -0,0 +1,37 @@
+// console.log("Diyorbek");
+const userNomi = "DiyorbekOlimov";
+let usernomi = "Diyorbek";
+// console.log(userNomi);
+// userNomi = "Qodirjon";
+// console.log(userNomi);
+
+// Arifmetic operators
+// let son1 = 24;
+// let son2 = 6;
+
+// console.log(son1 + son2); // 30
+// console.log(son1 - son2); // 18
+// console.log(son1 * son2); // 144
+// console.log(son1 / son2); // 4
+// console.log(son1 % son2); // 0
+// console.log(++son1); // 24 son1 = son1 + 1
+// console.log(son1);
+// console.log(++son2); // 7
+
+// console.log(--son1); // 23
+// console.log(son1--); // 23
+// console.log(son1); // 22
+
+// Comparison operators
+
+let son1 = 24;
+let son2 = "24";
+
+// console.log(son1 > son2); // true
+// console.log(son1 < son2); // false
+// console.log(son1 <= son2); // false
+// console.log(son1 >= son2); // true
+// console.log(son1 == son2); // true
+// console.log(son1 === son2); // false
+// console.log(son1 != son2); // false
+// console.log(son1 !== son2); // true
