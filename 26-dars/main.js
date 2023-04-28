7b857a9eedc48c9c8991e9de0de901c569bb96f1
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2023-04-28 22:37:37 +0500

    lesson 26

---
+++ 26-dars/main.js
@@ -0,0 +1,49 @@
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.SQRT2);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.37));
+// console.log((5.6734745683475).toFixed(1));
+// console.log(Math.ceil(5.00001));
+// console.log(Math.floor(5.9999));
+// console.log(Math.max(40, 34, 3, 99, -48, 567.8));
+// console.log(Math.min(40, 34, 3, 99, -48, 567.8));
+// console.log(Math.pow(8, 8));
+
+// const text = "Hello World!";
+
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(text.includes("d"));
+// console.log(text.indexOf("d"));
+// console.log(text.lastIndexOf("d"));
+// console.log(text.replace("World", "Diyorbek"));
+// console.log(text.split("lo"));
+// console.log(text.slice(7, -3));       // or
+// console.log(text.substring(-2, 4));   // Hell
+
+// let developer = {
+//   name: "Diyorbek",
+//   age: 23,
+//   experience: 3,
+// };
+
+// console.log(Object.values(developer));
+// console.log(Object.keys(developer));
+// console.log(Object.entries(developer));
+
+const arr1 = [34, 12, 76, 87, 43];
+const arr2 = [76, 45, -28, 99];
+
+function maxMin(arr) {
+  let arrMax = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arrMax < arr[i]) {
+      arrMax = arr[i];
+    }
+  }
+  console.log(arrMax);
+}
+
+maxMin(arr1);
